1ad95fa2c77ac15805af1245f7bb0500950d4e9c
prof_riemke <riemke-gurzki@hdm-stuttgart.de>
2024-10-08 15:59:50 +0200

    LV 08.19.24

---
+++ 01-GrundlagenJS/1.js
@@ -0,0 +1,7 @@
+console.log('hallo')
+let a = 5
+console.log(a*2)
+
+let b='5'
+
+console.log(a+b)
---
+++ 01-GrundlagenJS/2.js
@@ -0,0 +1,7 @@
+let a =5
+
+if(a == 7){
+    console.log('true')
+}else{
+    console.log('false')
+}
