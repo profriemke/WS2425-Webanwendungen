a4bba71378e9a0e8607bd1ef20ef0455058def69
Thorsten Riemke <gurzki@hdm-stuttgart.de>
2024-11-05 17:45:04 +0100

    LV 05.11 (2)

--- 04-adonisjs-grundlagen/erstes/start/routes.ts
+++ 04-adonisjs-grundlagen/erstes/start/routes.ts
@@ -9,9 +9,27 @@
 
 import router from '@adonisjs/core/services/router'
 
+router.post('/nutzer/anzeigen', async ({ view, request }) => {
+    const data = {
+        vorname: request.input('vorname'),
+        nachname: request.input('nachname')
+    }
+    console.log(data)
+    return view.render('pages/nutzer_anzeigen', data)
+
+
+})
+
+router.get('/nutzer', async ({ view }) => {
+    return view.render('pages/nutzer_form')
+
+})
+
+
 router.get('/', async ({ view }) => {
     //console.log(await view.render('pages/home'))
     const data = {
+        isLoggedIn: false,
         gruss: 'Guten Hase!', wochentag: 'Sonntag', zeit: new Date().toLocaleTimeString(),
         namen: ['Malte', 'Jaqueline', 'Horst'],
         personen: [
