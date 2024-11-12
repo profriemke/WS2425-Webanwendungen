23e5cf9106c548b517bf48314cc0bdc941544382
prof_riemke <2960904+profriemke@users.noreply.github.com>
2024-11-12 13:14:13 +0100

    LV 12.11

--- 04-adonisjs-grundlagen/erstes/start/routes.ts
+++ 04-adonisjs-grundlagen/erstes/start/routes.ts
@@ -9,15 +9,59 @@
 
 import router from '@adonisjs/core/services/router'
 
+let counter = 0
+
+let votes = {
+    spitze: 0,
+    gehtso: 0,
+    keinesfalls: 0
+}
+
+router.get('/vote/view', async ({ view })=>{
+    return view.render('pages/vote/view', votes)
+
+})
+
+router.post('/vote/count', async ({ view, request })=>{
+    console.log(request.input('vote'))
+    if(request.input('vote') == "gehtso"){
+        votes.gehtso++
+    }
+    if(request.input('vote') == "spitze" ){
+        votes.spitze++
+    }
+    if(request.input('vote') == "keinesfalls"){
+        votes.keinesfalls++
+    }
+    console.log(votes)
+    return view.render('pages/vote/danke')
+
+})
+
+router.get('/vote', async ({ view })=>{
+    return view.render('pages/vote/form')
+})
+
+
+router.get('/counter', async ({ view }) => {
+    counter++  //counter = counter +1
+    return view.render('pages/counter', { counter: counter })
+})
+
 router.post('/nutzer/anzeigen', async ({ view, request }) => {
+    if (!request.input('vorname')) {
+        return 'Fehler'
+    }
     const data = {
         vorname: request.input('vorname'),
         nachname: request.input('nachname')
     }
     console.log(data)
     return view.render('pages/nutzer_anzeigen', data)
-
-
+    // return view.render('pages/nutzer_anzeigen', {vorname:'Horst', nachname:'Meier'})
+    // let vorname='Horst'
+    //return view.render('pages/nutzer_anzeigen', { vorname: vorname })
+    //return view.render('pages/nutzer_anzeigen', { vorname })
 })
 
 router.get('/nutzer', async ({ view }) => {
