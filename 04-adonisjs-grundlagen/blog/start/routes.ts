fdd0a1c8d832bae772c313a658b96492956f1246
prof_riemke <2960904+profriemke@users.noreply.github.com>
2024-11-29 08:59:18 +0100

    LV 26.11.

--- 04-adonisjs-grundlagen/blog/start/routes.ts
+++ 04-adonisjs-grundlagen/blog/start/routes.ts
@@ -9,29 +9,65 @@
 
 import router from '@adonisjs/core/services/router'
 import db from '@adonisjs/lucid/services/db'
+import hash from '@adonisjs/core/services/hash'
 
-router.get('/', async ({ view }) => {
-    const posts = await db.from('posts')
-                          .select('*')
-                          .orderBy('id', 'desc')
-    return view.render('pages/home', { posts: posts })
+router.post('admin/login', async ({ session, request, response })=>{ if(session.get('login') === undefined){
+    response.redirect('/admin/login')
+}
+    const result = await db.from('users')
+                           .select('*')
+                           .where('login', request.input('login'))
+                           .first()
+    if(!result){
+        console.log('Nutzer nicht gefunden')
+        return response.redirect('/admin/login')
+    }    
+    console.log(result) 
+    const passwordOk = await hash.verify(result.password, request.input('password'))     
+    if(!passwordOk){
+        return response.redirect('/admin/login')
+    }    
+    session.put('login', result.login)
+    session.put('firstname', result.firstname)
+    session.put('lastname', result.lastname)
+    return response.redirect('/')         
+   
 })
 
-router.get('/posts/:id', async ({view, params })=>{
+router.get('admin/login', async ({ view })=>{
+    return view.render('pages/admin/login')
+})
+router.get('/admin/logout', async ({ session, response })=>{
+    session.clear()
+    return response.redirect('/')
+})
+
+
+router.get('/', async ({ view, session }) => {
+    const posts = await db.from('posts')
+        .select('*')
+        .orderBy('id', 'desc')
+    return view.render('pages/home', { posts: posts, firstname: session.get('firstname') })
+})
+
+router.get('/posts/:id', async ({ view, params }) => {
     const post = await db.from('posts')
-                         .select('*')
-                         .where({id: params.id })
-                         .first()
+        .select('*')
+        .where({ id: params.id })
+        .first()
     console.log(post)
     return view.render('pages/posts', post)
 })
 
-router.get('/admin/post/create', async({ view })=>{
+router.get('/admin/post/create', async ({ view, session, response }) => {
+    if(session.get('login') === undefined){
+        response.redirect('/admin/login')
+    }
     return view.render('pages/admin/createpost')
 })
 
-router.post('/admin/post/create', async ({ request, response })=>{
-    try{
+router.post('/admin/post/create', async ({ request, response }) => {
+    try {
         const result = await db.table('posts').insert(
             {
                 title: request.input('title'),
@@ -41,21 +77,31 @@ router.post('/admin/post/create', async ({ request, response })=>{
                 date: new Date().toLocaleDateString()
             }
         )
-        if(!result){
+        if (!result) {
             return 'Fehler!'
         }
-    }catch(error){
+    } catch (error) {
         return 'Fehler beim Eintragen'
     }
     return response.redirect('/')
 })
 
 
-router.get('/a', async ({ session }) => {
+router.get('/cookiedemo/a', async ({ session }) => {
     session.put('text', 'Riemke war da')
     return 'Wert gesetzt';
-  });
-  
-  router.get('/b', async ({ session }) => {
+});
+
+router.get('/cookiedemo/b', async ({ session }) => {
     return session.get('text');
-  });
+});
+router.get('/cookiedemo/c', async ({ session }) => {
+    session.forget('text')
+    return 'text gelöscht'
+});
+
+router.get('/hash', async () => {
+    const hashedPw = await hash.make('123')
+    console.log(hashedPw)
+    return await hash.verify(hashedPw, '123')
+});
