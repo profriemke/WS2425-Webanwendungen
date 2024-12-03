b49376c495c61b74027fef562155a9aa0a470380
prof_riemke <2960904+profriemke@users.noreply.github.com>
2024-12-03 18:50:20 +0100

    LVs 03.12.

--- 04-adonisjs-grundlagen/blog/start/routes.ts
+++ 04-adonisjs-grundlagen/blog/start/routes.ts
@@ -11,43 +11,75 @@ import router from '@adonisjs/core/services/router'
 import db from '@adonisjs/lucid/services/db'
 import hash from '@adonisjs/core/services/hash'
 
-router.post('admin/login', async ({ session, request, response })=>{ if(session.get('login') === undefined){
-    response.redirect('/admin/login')
-}
+router.post('/admin/post/edit', async ({ request, response, session }) => {
+    if (session.get('login') === undefined) {
+        response.redirect('/admin/login')
+    }
+    const result = await db.from('posts')
+        .where({ id: request.input('id') })
+        .update(
+            {
+                title: request.input('title'),
+                teaser: request.input('teaser'),
+                text: request.input('text'),
+                date: new Date().toLocaleDateString()
+            }
+        )
+    if (!result) {
+        return 'Fehler'
+    }
+    console.log(result)
+    return response.redirect('/posts/' + request.input('id'))
+})
+
+router.get('/admin/post/edit/:id', async ({ params, view, session, response }) => {
+    if (session.get('login') === undefined) {
+        response.redirect('/admin/login')
+    }
+    const post = await db.from('posts')
+        .select('*')
+        .where({ id: params.id })
+        .first()
+    return view.render('pages/admin/editpost', post)
+})
+
+router.post('admin/login', async ({ session, request, view, response }) => {
+
     const result = await db.from('users')
-                           .select('*')
-                           .where('login', request.input('login'))
-                           .first()
-    if(!result){
-        console.log('Nutzer nicht gefunden')
-        return response.redirect('/admin/login')
-    }    
-    console.log(result) 
-    const passwordOk = await hash.verify(result.password, request.input('password'))     
-    if(!passwordOk){
-        return response.redirect('/admin/login')
-    }    
+        .select('*')
+        .where('login', request.input('login'))
+        .first()
+    if (!result) {
+        console.error('Nutzer nicht gefunden')
+        return view.render('pages/admin/login', { error: 'Nutzer nicht gefunden' })
+    }
+    console.log(result)
+    const passwordOk = await hash.verify(result.password, request.input('password'))
+    if (!passwordOk) {
+        return view.render('pages/admin/login', { error: 'Passwort falsch' })
+    }
     session.put('login', result.login)
     session.put('firstname', result.firstname)
     session.put('lastname', result.lastname)
-    return response.redirect('/')         
-   
+    return response.redirect('/')
+
 })
 
-router.get('admin/login', async ({ view })=>{
+router.get('admin/login', async ({ view }) => {
     return view.render('pages/admin/login')
 })
-router.get('/admin/logout', async ({ session, response })=>{
+
+router.get('/admin/logout', async ({ session, response }) => {
     session.clear()
     return response.redirect('/')
 })
 
 
-router.get('/', async ({ view, session }) => {
+router.get('/', async ({ view }) => {
     const posts = await db.from('posts')
         .select('*')
         .orderBy('id', 'desc')
-    return view.render('pages/home', { posts: posts, firstname: session.get('firstname') })
+    return view.render('pages/home', { posts: posts })
 })
 
 router.get('/posts/:id', async ({ view, params }) => {
@@ -55,25 +87,40 @@ router.get('/posts/:id', async ({ view, params }) => {
         .select('*')
         .where({ id: params.id })
         .first()
+
+    if (!post) {
+        return view.render('pages/404')
+    }
+    const user = await db.from('users')
+        .select('*')
+        .where({ login: post.author })
+        .first()
     console.log(post)
+    post.firstname = user.firstname
+    post.lastname = user.lastname
     return view.render('pages/posts', post)
+    // {post:post, firstname: user.firstname, lastname:user.lastname}
+    // post.title   firstname
 })
 
 router.get('/admin/post/create', async ({ view, session, response }) => {
-    if(session.get('login') === undefined){
+    if (session.get('login') === undefined) {
         response.redirect('/admin/login')
     }
     return view.render('pages/admin/createpost')
 })
 
-router.post('/admin/post/create', async ({ request, response }) => {
+router.post('/admin/post/create', async ({ request, response, session }) => {
+    if (session.get('login') === undefined) {
+        response.redirect('/admin/login')
+    }
     try {
         const result = await db.table('posts').insert(
             {
                 title: request.input('title'),
                 teaser: request.input('teaser'),
                 text: request.input('teaser'),
-                author: request.input('author'),
+                author: session.get('login'),
                 date: new Date().toLocaleDateString()
             }
         )
